6a6697bfd4a2a9bf513c2c8f7249d757082a63ca
knifeworm <77696553+knifeworm@users.noreply.github.com>
2022-04-15 22:44:20 +0100

    Fixed some nooby errors in ban.py

--- cogs/ban.py
+++ cogs/ban.py
@@ -10,11 +10,13 @@ class ban(commands.Cog):
     @commands.has_guild_permissions(ban_members=True)
     async def ban(self, ctx, member : discord.Member = None, *, reason = None):
         if member == None:
+            await ctx.send("Please enter a member. e.g !ban @OddBot reason")
+        else:
             embed = discord.Embed(title="Banned Member", color=0xFF0000)
             embed.add_field(name="Member Banned", value=f"{member.name}#{member.discriminator}", inline=False)
             embed.add_field(name="Reason", value=f"{reason}", inline=False)
             embed.add_field(name="Time", value=today, inline=False)
-            await ctx.send(emebd=embed)
+            await ctx.send(embed=embed)
             memberEmbed = discord.Embed(title="You have been banned.", description=f"Guild: {ctx.guild.name}", color=0xFF0000)
             memberEmbed.add_field(name="Staff Member", value=f"{ctx.author.name}#{ctx.author.discriminator}", inline=False)
             memberEmbed.add_field(name="Reason", value=f"{reason}", inline=False)
@@ -25,8 +27,6 @@ class ban(commands.Cog):
             except discord.DiscordException:
                 await ctx.send("Member has not been informed that they have been banned. Due to there dms being closed.")
                 await member.ban(reason=reason)
-        else:
-            await ctx.send('Please enter a member. e.g: !ban @OddBot reason')
 
 def setup(bot):
     bot.add_cog(ban(bot))
