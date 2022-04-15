86562254014e47cb3781b352633097115378b99e
knifeworm <77696553+knifeworm@users.noreply.github.com>
2022-04-15 22:02:49 +0100

    Auto load cogs on bot start up

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import config
+import os
 
 bot = commands.Bot(command_prefix="!")
 
@@ -8,5 +9,13 @@ bot = commands.Bot(command_prefix="!")
 async def on_ready():
     print(f"{bot.user.name} is online")
 
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+        print("Cogs loaded")
+    else:
+        print("One cog failed to load.")
 
 bot.run(config.token)
