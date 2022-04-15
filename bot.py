2f4e3a151150722baf17e3e51c26b51e8c59faf2
knifeworm <77696553+knifeworm@users.noreply.github.com>
2022-04-15 22:07:52 +0100

    Added commands to reload load and unload cogs within discord

--- bot.py
+++ bot.py
@@ -10,6 +10,32 @@ async def on_ready():
     print(f"{bot.user.name} is online")
 
 
+@bot.command()
+async def load(ctx, extension):
+    if ctx.author.id == config.owner_id:
+        bot.load_extension(f'cogs.{extension}')
+        await ctx.send(f"Loaded Cog {extension}")
+    else:
+        await ctx.send("You are not the owner of this bot.")
+
+
+@bot.command()
+async def unload(ctx, extension):
+    if ctx.author.id == config.owner_id:
+        bot.unload_extension(f'cogs.{extension}')
+        await ctx.send(f"Unloaded Cog {extension}")
+    else:
+        await ctx.send("You are not the owner of this bot.")
+
+@bot.command()
+async def reload(ctx, extension):
+    if ctx.author.id == config.owner_id:
+        bot.reload_extension(f'cogs.{extension}')
+        await ctx.send(f"Reloaded Cog {extension}")
+    else:
+        await ctx.send("You are not the owner of this bot.")
+
+
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
